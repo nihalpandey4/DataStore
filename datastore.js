7ef3882ed79044f7c895bfe6e3e6556427369d11
nihalpandey4 <rockingnihal1305@gmail.com>
2020-12-31 23:43:32 +0530

    max file size check functionality added

--- datastore.js
+++ datastore.js
@@ -1,8 +1,10 @@
-const {LocalStorage} = require("node-localstorage");
+const { LocalStorage } = require("node-localstorage");
 const fs = require("fs");
 
 const DEFAULT_FILEPATH = "./Store";
 const MAX_KEY_LENGTH = 32;
+const MAX_FILE_SIZE = 1024; // In MB
+
 
 class DataStore {
   constructor(location) {
@@ -16,8 +18,13 @@ class DataStore {
         @param {obj}: value
         @param {integer}: timeToLive - default null
     */
-  add=(key, value) =>{
+  add = (key, value) => {
     try {
+
+      if (this.isMaxFileSizeExceeded()) {
+        throw new Error(`Maximum file size exceeded: ${MAX_FILE_SIZE}`);
+      }
+
       if (key.length > MAX_KEY_LENGTH) {
         throw new Error(`Key can be max ${MAX_KEY_LENGTH} characters!`);
       }
@@ -35,7 +42,10 @@ class DataStore {
     } catch (error) {
       console.log(error.message);
     }
-  }
+  };
+
+  //Function to keep file size under limit
+  isMaxFileSizeExceeded=()=> (fs.statSync(this.location).size / (1024 * 1024))>MAX_FILE_SIZE;
 
 }
 
