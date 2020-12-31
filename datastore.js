a438b20f78d2577e817b8b5f902029568f2579f9
nihalpandey4 <rockingnihal1305@gmail.com>
2020-12-31 23:25:49 +0530

    file not present exception resolved using local storage

--- datastore.js
+++ datastore.js
@@ -1,77 +1,42 @@
+const {LocalStorage} = require("node-localstorage");
 const fs = require("fs");
 
-class Datastore {
-  //initialising datastore with user given path or else default one
-  constructor(path = "default.json") {
-    this.path = path;
-    this.data = {};
-    
-    try {
-        const jsonData = JSON.parse(fs.readFileSync(this.path, "utf-8"));
-        this.data = { ...this.data, ...jsonData };
-    } 
-    catch (e) {
-      fs.writeFile(this.path, JSON.stringify(this.data), (err) => {
-        if (err) console.log(err);
-        console.log(
-          `File with path=${this.path} not present therefore creating new one`
-        );
-      });
-    }
+const DEFAULT_FILEPATH = "./Store";
+const MAX_KEY_LENGTH = 32;
 
+class DataStore {
+  constructor(location) {
+    this.location = location || DEFAULT_FILEPATH;
+    this.localstorage = new LocalStorage(this.location);
   }
 
-  //updating data property
-  updateData = () => {
-    const jsonData = JSON.parse(fs.readFileSync(this.path, "utf-8"));
-    this.data = { ...this.data, ...jsonData };
-    return this.data;
-  };
-
-  //reading value from data
-  readData = (key)=>{
-      this.updateData();
-      return this.data[key];
-  }
-
-  //writing into file
-  writeData = (newData) => {
-    console.log(newData);
-    let message = {};
-
-    if (!newData) {
-      message.error = "Empty object received";
-      return message;
-    }
-
+  /*
+        To add a key-value pair in local Database
+        @param {str}: key 
+        @param {obj}: value
+        @param {integer}: timeToLive - default null
+    */
+  add=(key, value) =>{
     try {
-      this.updateData();
-    } catch (e) {
-      console.log(e);
-    }
-
-    Object.keys(newData).forEach((key) => {
-      if (this.data[key]) {
-        message.error = `Aborting write operation as update is not allowed for property : "${key}"`;
+      if (key.length > MAX_KEY_LENGTH) {
+        throw new Error(`Key can be max ${MAX_KEY_LENGTH} characters!`);
       }
-    });
-    if (message.error) {
-      return message;
+
+      if (this.localstorage.getItem(key)) {
+        throw new Error("Key already exists.");
+      }
+
+      const jsonObject = JSON.stringify({
+        value,
+      });
+      this.localstorage.setItem(key, jsonObject);
+
+      console.log("Key-Value pair added.");
+    } catch (error) {
+      console.log(error.message);
     }
-
-    this.data = { ...this.data, ...newData };
-    fs.writeFile(this.path, JSON.stringify(this.data), (err) => {
-      if (err) console.log(err);
-      console.log("changes completed");
-    });
-    message.success = "Changes completed successfully"
-    return message;
-  };
-
-  deleteData =(key)=>{
-      
   }
-  
+
 }
 
-module.exports = Datastore;
+module.exports = DataStore;
